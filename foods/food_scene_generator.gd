646c88f00361131f492dd04f33592ea46b782316
DrHaid <40427630+DrHaid@users.noreply.github.com>
2024-12-20 22:10:23 +0100

    Some adjustments of food generation

--- foods/food_scene_generator.gd
+++ foods/food_scene_generator.gd
@@ -3,6 +3,7 @@ extends Node3D
 ## Utility script for generating foods scenes to be used in-game out of food resources  
 
 @export var foods: Array[FoodItemData] = []
+@export var model_scale: float = 0.6
 @export_dir var output_path: String = ""
 
 func _ready() -> void:
@@ -23,24 +24,21 @@ func generate_food(food: FoodItemData) -> void:
 	var imported_fbx := load(food.model)
 	var meshes := get_meshes(imported_fbx)
 	for mesh in meshes:
-		# reparent mesh instances from imported FBX to foodRigidBody
-		mesh.owner = null
-		mesh.reparent(food_rigidbody)
-		mesh.owner = food_rigidbody
-
+		_reparent(mesh, food_rigidbody)
 		# create static body collision shape
+		mesh.scale = Vector3.ONE * model_scale
 		mesh.create_convex_collision()
 		var static_child := mesh.get_child(0)
 		var collision_shape := static_child.get_child(0)
 		# make collision shape child of rigidbody
-		collision_shape.owner = null
-		collision_shape.reparent(food_rigidbody)
-		collision_shape.owner = food_rigidbody
+		_reparent(collision_shape, food_rigidbody)
+		collision_shape.scale = Vector3.ONE * model_scale
 		# exclude static body from being saved
 		static_child.owner = null
 
 	save_node(food_rigidbody, food.name.to_lower().replace(" ", "_"))
 	print("Saved: %s" % food.name)
+	get_tree().quit()
 
 
 func get_meshes(packed_scene: PackedScene) -> Array[MeshInstance3D]:
@@ -50,10 +48,16 @@ func get_meshes(packed_scene: PackedScene) -> Array[MeshInstance3D]:
 		if is_instance_of(child, MeshInstance3D):
 			meshes.append(child)
 	return meshes
-		
-	
+
+
 func save_node(node: Node3D, scene_name: String) -> void:
 	var scene := PackedScene.new()
 	scene.pack(node)
 	var path := "%s/%s.tscn" % [output_path, scene_name]
 	ResourceSaver.save(scene, path)
+
+
+func _reparent(child: Node3D, parent: Node3D) -> void:
+		child.owner = null
+		child.reparent(parent)
+		child.owner = parent
