c341b8d6822b12a80b00523642d4f30c5ebb64d9
DrHaid <40427630+DrHaid@users.noreply.github.com>
2025-05-13 21:59:43 +0200

    Revert "Scale food while cooking" (i didn't like it)

--- foods/food.gd
+++ foods/food.gd
@@ -7,15 +7,12 @@ var drag_food_target: Marker3D = null
 var is_cooking: bool = false
 
 var original_position: Vector3 = Vector3.ZERO
-var original_scale: Vector3 = Vector3.ZERO
 
 func _ready() -> void:
-	can_sleep = false
 	var food_drag_handler := get_tree().current_scene.get_node("%FoodDragHandler")
 	food_drag_handler.connect("start_drag", _on_start_drag)
 	food_drag_handler.connect("end_drag", _on_end_drag)
 	original_position = global_position
-	original_scale = scale
 
 func _process(_delta: float) -> void:
 	if drag_food_target:
@@ -35,16 +32,13 @@ func _on_end_drag(dragged_object: Node3D) -> void:
 		drag_food_target = null
 		freeze = false
 
-func set_cooking(cooking: bool, speed: int = 0) -> void:
+func set_cooking(cooking: bool) -> void:
 	is_cooking = cooking
-	if cooking:
-		var stew_tween := create_tween()
-		stew_tween.tween_property(self, "scale", Vector3.ZERO, speed) # TODO: tweening scale here not enough, also tween scale of collider
-		stew_tween.finished.connect(reset_food)
+	can_sleep = not is_cooking #HACK: necessary for some reason or rigidbody will fall asleep while moving?
+	set_collision_mask_value(1, not is_cooking)
+	set_collision_mask_value(3, is_cooking)
 
 func reset_food() -> void:
 	set_cooking(false)
-	scale = original_scale
 	global_position = original_position
 	linear_velocity = Vector3.ZERO
-	angular_velocity = Vector3.ZERO
